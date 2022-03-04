a0f750c804c8e6899337104ff06f8e038bb60fdd
Dawn LeBlanc <dawnleblanc11@gmail.com>
2022-03-04 13:45:18 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  // sets up the image to be blank when entering screen
+  const [baseImage, setbaseImage] = useState("");
+  
+  // the event when click upload file creates a large output, we will pull just the area with key file info
+  const uploadImage = async (e) => {
+  const file = e.target.files[0];
+  // the uploaded file will be converted to Base64 in order to be saved to MONGO
+  const base64 = await convertBase64(file);
+  // that will set the baseImage as the coverted file
+  console.log(base64);
+  // left this in to show what the base64 will look like in database
+      // may need to take off everything before comma? 
+      // if so, use .splt(",")[1] 
+  setbaseImage(base64)
+  };
+
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) =>{
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      
+
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={(e) => {
+        uploadImage(e);
+      }}
+      />
+      <br></br>
+      <img src={baseImage} height = "200px" alt="postphoto"/>
     </div>
   );
 }
